7d42d3b0f6fdf2b66a0adae990952be80b682287
adogbeji <baka_1993@yahoo.co.uk>
2024-07-03 13:40:46 +0100

    Add gradient background to start screen

--- lib/views/screens/start_screen.dart
+++ lib/views/screens/start_screen.dart
@@ -7,13 +7,33 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text('Task Planner', style: TextStyle(fontSize: 24,),),
-        ],
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.pink,
+            Colors.red,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Task Planner', style: TextStyle(fontSize: 24,),),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.arrow_forward),
+                Text('Start'),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
