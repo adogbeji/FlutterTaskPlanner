e7b3f76d2faef80484af995048b62ebc457e8e32
adogbeji <baka_1993@yahoo.co.uk>
2024-08-01 12:49:39 +0100

    Use Dismissible widget in TasksList widget

--- lib/views/widgets/tasks_list.dart
+++ lib/views/widgets/tasks_list.dart
@@ -4,7 +4,8 @@ import 'package:task_planner/models/task.dart';
 import 'package:task_planner/views/widgets/task_item.dart';
 
 class TasksLists extends StatelessWidget {
-  const TasksLists({super.key, required this.tasks, required this.onRemoveTask});
+  const TasksLists(
+      {super.key, required this.tasks, required this.onRemoveTask});
 
   final List<Task> tasks;
   final void Function(Task task) onRemoveTask;
@@ -13,7 +14,13 @@ class TasksLists extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: tasks.length,
-      itemBuilder: (context, index) => TaskItem(tasks[index]),
+      itemBuilder: (context, index) => Dismissible(
+        key: ValueKey(tasks[index]),
+        onDismissed:(direction) {
+          onRemoveTask(tasks[index]);
+        },
+        child: TaskItem(tasks[index]),
+      ),
     );
   }
 }
